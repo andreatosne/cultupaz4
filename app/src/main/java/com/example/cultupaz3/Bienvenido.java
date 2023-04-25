75b8a3f2de5c94b51d726cd845fe2a2de80ef475
apquinonez9 <apquinonez9@misena.edu.co>
2023-04-25 09:34:35 -0500

    AgregO Vista Bienvenidos Y Termios Y Condiciones

--- app/src/main/java/com/example/cultupaz3/Bienvenido.java
+++ app/src/main/java/com/example/cultupaz3/Bienvenido.java
@@ -2,13 +2,25 @@ package com.example.cultupaz3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Bienvenido extends AppCompatActivity {
-
+    Button btn_empezar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenido);
+
+        btn_empezar = findViewById(R.id.btn_empezar);
+        btn_empezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irsiguiente=new Intent(Bienvenido.this,Terminosycondiciones.class);
+                startActivity(irsiguiente);
+            }
+        });
     }
 }
