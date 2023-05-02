c02bc4b98bf157daf0f6c97dfc8a3a3e294cb455
apquinonez9 <apquinonez9@misena.edu.co>
2023-05-02 08:20:38 -0500

    Agrego cambio en manifiesto para la no rotación en Vista Bienvenidos Y Termios Condiciones,tambien agrego java a Terminos Y Condiciones

--- app/src/main/java/com/example/cultupaz3/Bienvenido.java
+++ app/src/main/java/com/example/cultupaz3/Bienvenido.java
@@ -6,15 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Bienvenido extends AppCompatActivity {
     Button btn_empezar;
+    ImageView img_logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenido);
 
         btn_empezar = findViewById(R.id.btn_empezar);
+
+        btn_empezar = (Button) findViewById(R.id.btn_empezar);
+
         btn_empezar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
