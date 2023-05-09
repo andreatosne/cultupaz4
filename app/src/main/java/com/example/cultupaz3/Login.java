9f9280d5949e6c75984351b0d376eae82e409842
apquinonez9 <apquinonez9@misena.edu.co>
2023-05-09 11:46:19 -0500

    Agrego Login y Menu

--- app/src/main/java/com/example/cultupaz3/Login.java
+++ app/src/main/java/com/example/cultupaz3/Login.java
@@ -1,14 +1,38 @@
 package com.example.cultupaz3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
 
 public class Login extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
