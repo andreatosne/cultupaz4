ed65b2caedc5293778a82a81b7db0e43556b5a45
apquinonez9 <apquinonez9@misena.edu.co>
2023-05-16 08:42:33 -0500

    conexión en vista principal

--- app/src/main/java/com/example/cultupaz3/vistaprincipal.java
+++ app/src/main/java/com/example/cultupaz3/vistaprincipal.java
@@ -2,13 +2,42 @@ package com.example.cultupaz3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class vistaprincipal extends AppCompatActivity {
 
+    LinearLayout tem1,tem2,tem3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vistaprincipal);
+
+        tem1=findViewById(R.id.tem1);
+
+
+
+        tem2 = findViewById(R.id.tem2);
+        tem3 =findViewById(R.id.tem3);
+
+
+        tem1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irsiguiente=new Intent(vistaprincipal.this,Principalblog.class);
+                startActivity(irsiguiente);
+            }
+        });
+        tem2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irsiguiente=new Intent(vistaprincipal.this,Muro.class);
+                startActivity(irsiguiente);
+            }
+        });
+
     }
 }
