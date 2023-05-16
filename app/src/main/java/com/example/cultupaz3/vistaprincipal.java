f418f827ad262d74855d2bc78777f970ab8b8639
apquinonez9 <apquinonez9@misena.edu.co>
2023-05-16 12:47:34 -0500

    correccion

--- app/src/main/java/com/example/cultupaz3/vistaprincipal.java
+++ app/src/main/java/com/example/cultupaz3/vistaprincipal.java
@@ -5,36 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.widget.ImageView;
 
-public class vistaprincipal extends AppCompatActivity {
-
-    ImageView inforcraft;
-
-    ImageView postmuro;
-
-    ImageView artesan;
-
-=======
 import android.view.View;
-import android.widget.ImageView;
+
 import android.widget.LinearLayout;
+
 
 public class vistaprincipal extends AppCompatActivity {
 
     LinearLayout tem1,tem2,tem3;
->>>>>>> ed65b2caedc5293778a82a81b7db0e43556b5a45
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vistaprincipal);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-<<<<<<< HEAD
 
-
-=======
         tem1=findViewById(R.id.tem1);
         tem2 = findViewById(R.id.tem2);
         tem3 =findViewById(R.id.tem3);
@@ -53,7 +40,7 @@ public class vistaprincipal extends AppCompatActivity {
                 startActivity(irsiguiente);
             }
         });
->>>>>>> ed65b2caedc5293778a82a81b7db0e43556b5a45
+
 
     }
 }
