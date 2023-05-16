d5b81b8d7d35f48372de40c5790b1b1f77dabfc3
yeisonAnte <yeison.ante@misena.edu.co>
2023-05-16 08:57:11 -0500

    principal

--- app/src/main/java/com/example/cultupaz3/vistaprincipal.java
+++ app/src/main/java/com/example/cultupaz3/vistaprincipal.java
@@ -3,12 +3,23 @@ package com.example.cultupaz3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class vistaprincipal extends AppCompatActivity {
+
+    ImageView inforcraft;
+
+    ImageView postmuro;
+
+    ImageView artesan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vistaprincipal);
+
+
+
+
     }
 }
